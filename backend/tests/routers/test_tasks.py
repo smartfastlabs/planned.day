3ee1aa533d06f36e503e33393b22c9772a786cf6
Todd Sifleet <todd.sifleet@gmail.com>
2025-11-29 19:13:17 -0600

    oops

--- backend/tests/routers/test_tasks.py
+++ backend/tests/routers/test_tasks.py
@@ -4,6 +4,6 @@ from planned.utils.dates import get_current_date
 
 
 @pytest.mark.asyncio
-async def test_get_todays(test_client, test_date):
+async def test_get_todays(test_client, test_date, clear_repos):
     result = test_client.get("/tasks/today")
     assert result.json() == []
--- backend/tests/services/test_planning.py
+++ backend/tests/services/test_planning.py
@@ -4,11 +4,10 @@ from planned.services import planning_svc
 
 
 @pytest.mark.asyncio
-async def test_schedule_today(test_date):
+async def test_schedule_today(test_date, clear_repos):
     result = await planning_svc.schedule_day(test_date)
     assert len(result.events) == 1
 
     assert result.events[0].name == "Sifleet Family Thanksgiving"
-
 
     assert len(result.tasks) == 2
